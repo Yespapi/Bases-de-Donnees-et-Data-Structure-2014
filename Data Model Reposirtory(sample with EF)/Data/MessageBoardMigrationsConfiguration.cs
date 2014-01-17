ed4ddff2ecb3547b19fd2eb30d590bd4c664ea51
aly ba <alybas2002@yahoo.fr>
2014-01-17 19:23:34 +0100

    entity framework, and some concept of database
    desgin à suivre

---
+++ Data Model Reposirtory(sample with EF)/Data/MessageBoardMigrationsConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+
+namespace MessageBoard.Data
+{
+    public  class MessageBoardMigrationsConfiguration  :DbMigrationsConfiguration<MessageBoardContext>
+    {
+        public MessageBoardMigrationsConfiguration()
+        {
+            this.AutomaticMigrationDataLossAllowed = true;
+            this.AutomaticMigrationsEnabled =true;
+        }
+        protected override void Seed(MessageBoardContext context)
+        {
+            base.Seed(context);
+        } 
+    }
+}
